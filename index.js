4d8bb2278e623d2e888b2783d83c89c7821cd3c1
IshaanDasgupta <dasgupta.ishan@gmail.com>
2024-08-29 11:15:17 +0530

    tweaking cors

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const connect = async () => {
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "*" }));
+app.use(cors());
 
 app.use("/api/user", userRouter);
 app.use("/api/help", helpRouter);
