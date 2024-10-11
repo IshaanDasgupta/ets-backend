f9725fa6762edf30d66f8b5236abf68687a6724e
anshjain16 <anshjain0216@gmail.com>
2024-10-11 09:25:05 +0530

    history & profile

--- routes/help.js
+++ routes/help.js
@@ -3,6 +3,7 @@ const {
     get_related_help_requests,
     reject_help,
     get_accepted_help,
+    get_user_history
 } = require("../controllers/help.js");
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.get("/all", get_related_help_requests);
 router.get("/", get_accepted_help);
 router.delete("/", reject_help);
+router.get("/history", get_user_history);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { get_user, send_otp, verify_otp } = require("../controllers/user.js");
+const { get_user, send_otp, verify_otp , get_user_by_phone_number} = require("../controllers/user.js");
 
 const router = Router();
 
 router.get("/", get_user);
+router.get('/phone', get_user_by_phone_number);
 router.post("/send-otp", send_otp);
 //sign in
 router.post("/verify-otp", verify_otp)
